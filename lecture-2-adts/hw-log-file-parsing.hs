71765cc0cbc56eab56607248b5d1ce999f20fb53
Alyssa Biasi <alyssa.biasi@rea-group.com>
2018-04-06 16:59:47 +1000

    Session 2 - parseInt & parseSeverity

--- lecture-2-adts/hw-log-file-parsing.hs
+++ lecture-2-adts/hw-log-file-parsing.hs
@@ -1,12 +1,14 @@
 
 module NewModule where
 
+import Text.Read
+
 data MessageType = Info
                  | Warning
                  | Error Severity
                  deriving (Show, Eq)
 
-type Severity = Int
+newtype Severity = Severity Int deriving (Show, Eq)
 
 parseWord :: String -> Maybe (String, String)
 parseWord "" = Nothing
@@ -16,16 +18,18 @@ parseWord input =
     go word (' ':r) = (reverse word, r)
     go word (c:r) = go (c:word) r
   in  Just (go [] input)
---parseWord (' ':r) =
---parseWord (c:r) = Just (c : parseWord(r), r)
-  
+
+parseInt :: String -> Maybe (Int, String)
+parseInt s = fmap convert parseWord s
+  where convert :: Maybe (String, String) -> Maybe (Int, String)
+        convert (Just (word, r)) = fmap (\i -> (i, r)) (readMaybe word)
+        convert Nothing = Nothing
+
+parseSeverity :: String -> Maybe (Severity, String)
+parseSeverity = fmap (\(i, s) -> (Severity i, s)) . parseInt
 
 --parseMessageType :: String -> Maybe (MessageType, String)
 --parseMessageType ("I":r) = Just (Info, r)
 --parseMessageType ("W":r) = Just (Warning, r)
 --parseMessageType ("E":r) = parseSeverity
-
-
---parseSeverity :: String -> Maybe (Severity, String)
---parseSeverity
 
