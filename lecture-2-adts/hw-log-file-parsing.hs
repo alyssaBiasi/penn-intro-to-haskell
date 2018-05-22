1c5565decd18e7a56135ba98b4a639cf7d7225f4
Alyssa Biasi <biasi91@gmail.com>
2018-05-22 13:54:48 +1000

    Session 3 - Application instance and Monad instance wip

--- lecture-2-adts/hw-log-file-parsing.hs
+++ lecture-2-adts/hw-log-file-parsing.hs
@@ -1,38 +1,57 @@
-
+{-# LANGUAGE DeriveFunctor #-}
 module NewModule where
 
-import Text.Read
+import qualified Text.Read as TR
+import Control.Monad
 
 data MessageType = Info
                  | Warning
                  | Error Severity
                  deriving (Show, Eq)
 
-newtype Severity = Severity Int deriving (Show, Eq)
+newtype Severity = Severity Int deriving (Show, Eq, Ord)
 
 newtype Parser a = Parser (String -> Maybe (a, String))
 
 instance Functor Parser where
   --fmap :: (a -> b) -> Parser a -> Parser b
-  fmap = undefined
+  fmap f (Parser parserF) = Parser $ \s -> fmap mapFirst (parserF s)
+    where mapFirst (a, r) = (f a, r)
+
+instance Applicative Parser where
+--  pure :: a -> Parser a
+  pure a = Parser $ \s -> Just (a, s)
+  -- (<*>) :: Parser a -> Parser (a -> b) -> Parser b
+  (Parser fa) <*> (Parser fab) = Parser $ \s -> (fa s) >>= convert
+    where convert :: (a, String) -> Maybe (b, String)
+          convert (a, r) = fmap (\(fb, w) -> (fb a, w)) (fab r)
+
+instance Monad Parser where
+-- (>>=) :: Parser a -> (a -> Parse b) -> Parser b
+  (>>=) = undefined
+
 
 parseWord :: Parser String
-parseWord "" = Nothing
-parseWord input = 
+parseWord =
   let
     go word [] = (reverse word, [])
     go word (' ':r) = (reverse word, r)
     go word (c:r) = go (c:word) r
-  in  Just (go [] input)
+  in  Parser $ \s -> Just (go [] s)
 
 parseInt :: Parser Int
-parseInt s = fmap convert parseWord s
-  where convert :: Maybe (String, String) -> Maybe (Int, String)
-        convert (Just (word, r)) = fmap (\i -> (i, r)) (readMaybe word)
-        convert Nothing = Nothing
+parseInt = Parser $ \s -> (parse s parseWord) >>= convert
+  where convert :: (String, String) -> Maybe (Int, String)
+        convert (word, r) = fmap (\i -> (i, r)) (TR.readMaybe word)
 
 parseSeverity :: Parser Severity
-parseSeverity = fmap (\(i, s) -> (Severity i, s)) . parseInt
+parseSeverity = fmap Severity parseInt
+
+parse :: String -> Parser a -> Maybe (a, String)
+parse s (Parser f) = f s
+
+
+
 
 --parseMessageType :: String -> Maybe (MessageType, String)
 --parseMessageType ("I":r) = Just (Info, r)
